448fe39a495654ab9efe049f15a9e6bbe5ddd51b
Arthur Novaes <arthurnovaes9@gmail.com>
2019-03-22 16:13:56 -0300

    feature/1 Update issues context tests
    -
    
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- test/sustentation/issues/issues_test.exs
+++ test/sustentation/issues/issues_test.exs
@@ -1,4 +1,5 @@
 defmodule Sustentation.IssuesTest do
+  import Sustentation.Factories
   use Sustentation.DataCase
 
   alias Sustentation.Issues
@@ -6,26 +7,20 @@ defmodule Sustentation.IssuesTest do
   describe "categories" do
     alias Sustentation.Issues.Category
 
+    setup do
+      category = insert(:category)
+      {:ok, category: category}
+    end
+
     @valid_attrs %{description: "some description", name: "some name"}
     @update_attrs %{description: "some updated description", name: "some updated name"}
     @invalid_attrs %{description: nil, name: nil}
 
-    def category_fixture(attrs \\ %{}) do
-      {:ok, category} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Issues.create_category()
-
-      category
-    end
-
-    test "list_categories/0 returns all categories" do
-      category = category_fixture()
+    test "list_categories/0 returns all categories", %{category: category} do
       assert Issues.list_categories() == [category]
     end
 
-    test "get_category!/1 returns the category with given id" do
-      category = category_fixture()
+    test "get_category!/1 returns the category with given id", %{category: category} do
       assert Issues.get_category!(category.id) == category
     end
 
@@ -39,27 +34,23 @@ defmodule Sustentation.IssuesTest do
       assert {:error, %Ecto.Changeset{}} = Issues.create_category(@invalid_attrs)
     end
 
-    test "update_category/2 with valid data updates the category" do
-      category = category_fixture()
+    test "update_category/2 with valid data updates the category", %{category: category} do
       assert {:ok, %Category{} = category} = Issues.update_category(category, @update_attrs)
       assert category.description == "some updated description"
       assert category.name == "some updated name"
     end
 
-    test "update_category/2 with invalid data returns error changeset" do
-      category = category_fixture()
+    test "update_category/2 with invalid data returns error changeset", %{category: category} do
       assert {:error, %Ecto.Changeset{}} = Issues.update_category(category, @invalid_attrs)
       assert category == Issues.get_category!(category.id)
     end
 
-    test "delete_category/1 deletes the category" do
-      category = category_fixture()
+    test "delete_category/1 deletes the category", %{category: category} do
       assert {:ok, %Category{}} = Issues.delete_category(category)
       assert_raise Ecto.NoResultsError, fn -> Issues.get_category!(category.id) end
     end
 
-    test "change_category/1 returns a category changeset" do
-      category = category_fixture()
+    test "change_category/1 returns a category changeset", %{category: category} do
       assert %Ecto.Changeset{} = Issues.change_category(category)
     end
   end
