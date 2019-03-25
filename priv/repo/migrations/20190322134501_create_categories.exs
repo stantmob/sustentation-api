08f8479804dd405d3de4c161957eeb7cc67ad87d
Arthur Novaes <arthurnovaes9@gmail.com>
2019-03-25 14:42:11 -0300

    feature/1 Add correct size on categories fields
    -
    
    Signed-off-by: Arthur Novaes <arthurnovaes9@gmail.com>

--- priv/repo/migrations/20190322134501_create_categories.exs
+++ priv/repo/migrations/20190322134501_create_categories.exs
@@ -3,11 +3,10 @@ defmodule Sustentation.Repo.Migrations.CreateCategories do
 
   def change do
     create table(:categories) do
-      add :name, :string, size: ?, null: false
-      add :description, :string, size: ?
+      add :name, :string, size: 254, null: false
+      add :description, :string, size: 10000
 
       timestamps()
     end
-
   end
 end
